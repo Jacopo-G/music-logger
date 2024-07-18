081016e1936e08002798c8dd6f21214201fff3de
Jacopo-G <jacopo.galardi@tutanota.com>
2024-07-18 19:43:01 +0200

    Bug Fixes, list order reversal & improved UI

--- lib/pages/history.dart
+++ lib/pages/history.dart
@@ -5,8 +5,6 @@ import 'package:image_picker/image_picker.dart';
 import 'package:music_logger/util/media_tile.dart';
 import 'package:music_logger/util/utils.dart';
 
-List<MediaTile> mediaList = [];
-
 class History extends StatefulWidget {
   const History({super.key});
 
@@ -15,40 +13,61 @@ class History extends StatefulWidget {
 }
 
 class _HistoryState extends State<History> {
+  List<MediaTile> mediaList = [];
+
+  void addMediaTile(MediaTile tile) {
+    setState(() {
+      mediaList.add(tile);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.deepPurple[700],
-        appBar: AppBar(
-          title: Text('History'),
-          backgroundColor: Colors.deepPurple[900],
-          titleTextStyle: TextStyle(
-            fontSize: 24,
-            fontWeight: FontWeight.bold,
-            color: Colors.white,
-          ),
-          elevation: 0,
+      backgroundColor: Colors.deepPurple[700],
+      appBar: AppBar(
+        title: Text('History'),
+        backgroundColor: Colors.deepPurple[900],
+        titleTextStyle: TextStyle(
+          fontSize: 24,
+          fontWeight: FontWeight.bold,
+          color: Colors.white,
         ),
-        body: GridView.count(
-          crossAxisCount: 1,
-          childAspectRatio: 10 / 2,
-          children: mediaList,
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () async {
-            await showDialog<void>(
-                context: context,
-                builder: (context) {
-                  return PopupDialog();
-                });
-          },
-          child: Icon(Icons.add),
-          backgroundColor: Colors.purpleAccent[0],
-        ));
+        elevation: 0,
+      ),
+      body: mediaList.isEmpty
+          ? Center(
+              child:
+                  Text('No items yet', style: TextStyle(color: Colors.white)))
+          : GridView.builder(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 1,
+                childAspectRatio: 10 / 2,
+              ),
+              itemCount: mediaList.length,
+              itemBuilder: (context, index) => mediaList.reversed.toList()[index],
+            ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          await showDialog<void>(
+            context: context,
+            builder: (context) {
+              return PopupDialog(onAdd: addMediaTile);
+            },
+          );
+        },
+        child: Icon(Icons.add),
+        backgroundColor: Colors.purpleAccent[0],
+      ),
+    );
   }
 }
 
 class PopupDialog extends StatefulWidget {
+  final Function(MediaTile) onAdd;
+
+  PopupDialog({required this.onAdd});
+
   @override
   _PopupDialogState createState() => _PopupDialogState();
 }
@@ -60,108 +79,87 @@ class _PopupDialogState extends State<PopupDialog> {
   TextEditingController artistNameEditingController = TextEditingController();
 
   void selectImage() async {
-    Uint8List img = await pickImage(ImageSource.gallery);
-    setState(() {
-      _image = img;
-    });
+    Uint8List? img = await pickImage(ImageSource.gallery);
+    if (img != null) {
+      setState(() {
+        _image = img;
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
       content: SingleChildScrollView(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Container(
-              child: Form(
-                  key: _formKey,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      TextFormField(
-                        controller: songTitleEditingController,
-                        decoration: InputDecoration(
-                          icon: Icon(Icons.music_note),
-                          labelText: 'Song Title *',
-                        ),
-                        validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            return 'Please enter song title';
-                          }
-                          return null;
-                        },
-                      ),
-                      TextFormField(
-                        controller: artistNameEditingController,
-
-                        decoration: InputDecoration(
-                          icon: Icon(Icons.person),
-                          labelText: 'Artist Name *',
-                        ),
-                        validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            return 'Please enter artist name';
-                          }
-                          return null;
-                        },
-                      ),
-                      Stack(children: [
-                        Padding(
-                          padding: const EdgeInsets.all(10),
-                          child: _image != null
-                              ? ClipRRect(
-                                  borderRadius: BorderRadius.circular(7),
-                                  child: GestureDetector(
-                                    onTap: selectImage,
-                                    child: Image(
-                                      height: 150,
-                                      fit: BoxFit.contain,
-                                      image: MemoryImage(_image!),
-                                      
-                                    ),
-                                  ))
-                              : ClipRRect(
-                                  borderRadius: BorderRadius.circular(7),
-                                  child: GestureDetector(
-                                    onTap: selectImage,
-                                    child: Image(
-                                      height: 150,
-                                      fit: BoxFit.contain,
-                                      image: NetworkImage(
-                                          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1BhBgvAdx2cQwiyvb-89VbGVzgQbB983tfw&s'),
-                                    ),
-                                  ),
-                                ),
-                        ),
-                      ])
-                    ],
-                  )),
-            ),
-          ],
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextFormField(
+                controller: songTitleEditingController,
+                decoration: InputDecoration(
+                  icon: Icon(Icons.music_note),
+                  labelText: 'Song Title *',
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter song title';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                controller: artistNameEditingController,
+                decoration: InputDecoration(
+                  icon: Icon(Icons.person),
+                  labelText: 'Artist Name *',
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter artist name';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 10),
+              GestureDetector(
+                onTap: selectImage,
+                child: Container(
+                  width: 150,
+                  height: 150,
+                  decoration: BoxDecoration(
+                    color: Colors.grey[200],
+                    borderRadius: BorderRadius.circular(7),
+                  ),
+                  child: _image != null
+                      ? Image.memory(_image!, fit: BoxFit.cover)
+                      : Icon(Icons.add_photo_alternate, size: 50),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
       actions: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            TextButton(
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-                child: Text("Cancel")),
-            TextButton(
-                onPressed: () {
-                  if (_formKey.currentState!.validate()) {
-                    setState(() {
-                    mediaList.add(MediaTile(songName: songTitleEditingController.text, artist: artistNameEditingController.text, imageSrc: _image));
-                    Navigator.of(context).pop();  
-                    });
-                    
-                  }
-                },
-                child: Text("Done"))
-          ],
+        TextButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          child: Text("Cancel"),
+        ),
+        TextButton(
+          onPressed: () {
+            if (_formKey.currentState!.validate()) {
+              widget.onAdd(MediaTile(
+                songName: songTitleEditingController.text,
+                artist: artistNameEditingController.text,
+                imageSrc: _image,
+              ));
+              Navigator.of(context).pop();
+            }
+          },
+          child: Text("Done"),
         ),
       ],
     );
